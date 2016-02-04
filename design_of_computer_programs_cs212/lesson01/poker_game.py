bcee47f2f7b8d6c040515b86e9a99fe3829cc05b
fernando_luz <fernando_luz@tpn.usp.br>
2016-02-04 08:33:58 -0200

    update the functions with none return, and improve hank_rank function

--- design_of_computer_programs_cs212/lesson01/poker_game.py
+++ design_of_computer_programs_cs212/lesson01/poker_game.py
@@ -7,4 +7,42 @@ def poker(hands):
 
 def hand_rank(hand):
     """define a rank for a specific hand"""
-    return None  # we will be changing this later.
+    ranks = card_ranks(hand)
+    if straight(ranks) and flush(hand):            # straight flush
+        return (8, max(ranks))
+    elif kind(4, ranks):                           # 4 of a kind
+        return (7, kind(4, ranks), kind(1, ranks))
+    elif kind(3, ranks) and kind(2, ranks):        # full house
+        return (6, kind(3, ranks), kind(2, ranks))
+    elif flush(hand):                              # flush
+        return (5, ranks)
+    elif straight(ranks):                          # straight
+        return (4, max(ranks))
+    elif kind(3, ranks):                           # 3 of a kind
+        return (3, kind(3, ranks), ranks)
+    elif two_pair(ranks):                          # 2 pair
+        return (2, two_pair(ranks), ranks)
+    elif kind(2, ranks):                           # kind
+        return (1, kind(2, ranks), ranks)
+    else:                                          # high card
+        return (0, ranks)
+
+
+def kind(number, hand):
+    return None
+
+
+def two_pair(ranks):
+    return None
+
+
+def flush(hand):
+    return None
+
+
+def card_ranks(hand):
+    return None
+
+
+def straight(ranks):
+    return None
