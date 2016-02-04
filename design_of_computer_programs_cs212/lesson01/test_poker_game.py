90341f6b1d0363fe4602e194c5ad2014c4c74fb2
fernando_luz <fernando_luz@tpn.usp.br>
2016-02-04 08:38:38 -0200

    update the tests, including the card_ranks tester

--- design_of_computer_programs_cs212/lesson01/test_poker_game.py
+++ design_of_computer_programs_cs212/lesson01/test_poker_game.py
@@ -9,7 +9,7 @@ class TddPokerGame(unittest.TestCase):
     """ Class to test poker game program"""
 
     def test_winner(self):
-        """ doc """
+        """ Test the return of the best hand """
         sf = "6C 7C 8C 9C TC".split()
         fk = "9D 9H 9S 9C 7D".split()
         fh = "TD TC TH 7C 7D".split()
@@ -31,3 +31,13 @@ class TddPokerGame(unittest.TestCase):
         self.assertEqual(poker_game.hand_rank(sf), (8, 10))
         self.assertEqual(poker_game.hand_rank(fk), (7, 9, 7))
         self.assertEqual(poker_game.hand_rank(fh), (6, 10, 7))
+
+    def test_card_ranks(self):
+        """ Test the card ranks"""
+        sf = "6C 7C 8C 9C TC".split()
+        fk = "9D 9H 9S 9C 7D".split()
+        fh = "TD TC TH 7C 7D".split()
+
+        self.assertEqual(poker_game.card_ranks(sf), [10, 9, 8, 7, 6])
+        self.assertEqual(poker_game.card_ranks(fk), [9, 9, 9, 9, 7])
+        self.assertEqual(poker_game.card_ranks(fh), [10, 10, 10, 7, 7])
