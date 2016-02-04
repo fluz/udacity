9f14640aec55c2bb853d6ea98e6ec7a449060868
fernando_luz <fernando_luz@tpn.usp.br>
2016-02-04 08:29:57 -0200

    update the cases tests to check hand_rank

--- design_of_computer_programs_cs212/lesson01/test_poker_game.py
+++ design_of_computer_programs_cs212/lesson01/test_poker_game.py
@@ -8,14 +8,26 @@ import poker_game
 class TddPokerGame(unittest.TestCase):
     """ Class to test poker game program"""
 
-    def test_case(self):
+    def test_winner(self):
         """ doc """
+        sf = "6C 7C 8C 9C TC".split()
+        fk = "9D 9H 9S 9C 7D".split()
+        fh = "TD TC TH 7C 7D".split()
+
+        self.assertEqual(
+            poker_game.poker([fk, sf, fh]), fh)
+        self.assertEqual(poker_game.poker([fk, fh]), fh)
+        self.assertEqual(poker_game.poker([fk, fk]), fk)
+        self.assertEqual(poker_game.poker([fk]), fk)
+        self.assertEqual(poker_game.poker([fk] * 99 + [sf]), sf)
+
+    def test_hand_rank(self):
+        """ Test the output of a hand_rank"""
 
         sf = "6C 7C 8C 9C TC".split()
         fk = "9D 9H 9S 9C 7D".split()
         fh = "TD TC TH 7C 7D".split()
 
-        self.assertEqual(poker_game.poker([fk, sf, fh]), fh)
-        self.assertEqual(poker_game.poker([fk, fh]), fh)
-        self.assertEqual(poker_game.poker([fk]), fk)
-        self.assertEqual(poker_game.poker([fk] * 99 + [sf]), sf)
+        self.assertEqual(poker_game.hand_rank(sf), (8, 10))
+        self.assertEqual(poker_game.hand_rank(fk), (7, 9, 7))
+        self.assertEqual(poker_game.hand_rank(fh), (6, 10, 7))
